989170a9802a4da2d6041ff96b5f81a852f65389
tokyo3751 <tomizawa3751@gmail.com>
2022-05-17 04:03:27 +0000

    [Add]ジャンル機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
-
  devise_for :administrator, skip: [:registrations, :passwords] ,controllers: {
   sessions: "administrator/sessions"
  }
+  namespace :administrator do
+   resources :genres, only: [:index,:create,:edit,:update]
+  end
+
   devise_for :customers
 end
