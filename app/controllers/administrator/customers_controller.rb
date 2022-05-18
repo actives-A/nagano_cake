3e0dec975fc5b9484c49c8cf034d3987913ee491
tokyo3751 <tomizawa3751@gmail.com>
2022-05-18 07:41:16 +0000

    [Add]会員一覧画面

--- app/controllers/administrator/customers_controller.rb
+++ app/controllers/administrator/customers_controller.rb
@@ -4,11 +4,24 @@ class Administrator::CustomersController < ApplicationController
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to administrator_customer_path
+    else
+      render :edit
+    end
   end
+
+  private
+   def customer_params
+     params.require(:customer).permit(:last_name,:first_name,:kana_last_name,:kana_first_name,:email,:postal_code,:address,:phone_namber,:encrypted_password,:is_deleted)
+   end
 end
