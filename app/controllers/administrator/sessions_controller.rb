0a1b4f031b58c6e43a9956431f19725d62c9e849
tokyo3751 <tomizawa3751@gmail.com>
2022-05-20 07:19:29 +0000

    [Add]トップ、ヘッダー追加

--- app/controllers/administrator/sessions_controller.rb
+++ app/controllers/administrator/sessions_controller.rb
@@ -18,8 +18,14 @@ class Administrator::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+   protected
+     def after_sign_in_path_for(resource)
+       administrator_orders_path
+     end
 
+     def after_sign_out_path_for(resource)
+       administrator_session_path
+     end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
-#Administrators.create!(
- #  email: 'admin@admin',
- #  password: 'testtest'
-#)
 
+Administrator.create!(
+  email: 'admin@admin',
+  password: 'testtest'
+)
 
- (1..10).each do |i|
-   user=Customer.new(email: i.to_s.to_s+"@"+i.to_s*3,password: 'hugahuga')
-   user.save!
- end
+ #(1..10).each do |i|
+   #user=Customer.new(email: i.to_s.to_s+"@"+i.to_s*3,password: 'hugahuga')
+   #user.save!
+# end
 
 # Administrators.create!(
 #    email: 'admin@admin',
@@ -25,34 +25,34 @@
 #   user.save!
 # end
 
-(1..10).each do |i|
-  Item.create(genre_id:1,name: "test#{i}",explanation: "test#{i}",out_tax_price: i*100,sales_status: true)
-end
-
-
 # (1..10).each do |i|
-#   Item.create(genre_id: i,name: "test#{i}",explanation: "test#{i}",out_tax_price: i*100,sales_status: true)
+#   Item.create(genre_id:1,name: "test#{i}",explanation: "test#{i}",out_tax_price: i*100,sales_status: true)
 # end
 
-(1..10).each do |i|
-  CartItem.create(item_id: i,customer_id: 1,quantity: i)
-end
+
+# # (1..10).each do |i|
+# #   Item.create(genre_id: i,name: "test#{i}",explanation: "test#{i}",out_tax_price: i*100,sales_status: true)
+# # end
 
 # (1..10).each do |i|
-#   Address.create(name: "ssss",customer_id: 1,postal_code: "ssssss",address: "1222333")
+#   CartItem.create(item_id: i,customer_id: 1,quantity: i)
 # end
+
+# # (1..10).each do |i|
+# #   Address.create(name: "ssss",customer_id: 1,postal_code: "ssssss",address: "1222333")
+# # end
+
+# # (1..2).each do |i|
+#   # Order.create(customer_id: i, created_at: "test#{i}",postal_code: "test#{i}",address: "test#{i}",send_name: "test#{i}",total_money: i*100, order_status: i)
+# # end
 
 # (1..2).each do |i|
-  # Order.create(customer_id: i, created_at: "test#{i}",postal_code: "test#{i}",address: "test#{i}",send_name: "test#{i}",total_money: i*100, order_status: i)
+#   OrderItem.create(item_id: 1, order_id: 45, quantity: i, intax_price: i*100, puroduction_status: 0)
 # end
 
-(1..2).each do |i|
-  OrderItem.create(item_id: 1, order_id: 45, quantity: i, intax_price: i*100, puroduction_status: 0)
-end
 
 
-
-  Item.create!(name: 'ケーキ',out_tax_price: '1000')
+  
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
