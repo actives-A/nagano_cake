d389e8eb383affaca58beb63f4224ee07c6f26f2
EC2 Default User <ec2-user@ip-172-31-29-63.ec2.internal>
2022-05-22 01:23:20 +0000

    [修正途中]

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -1,6 +1,6 @@
 class Customer::CustomersController < ApplicationController
   def show
-    @customer = Customer.find(params[:id])
+    @customer = current_customer
   end
 
   def unsubscribe
@@ -17,7 +17,7 @@ class Customer::CustomersController < ApplicationController
   end
 
   def edit
-    @customer = Customer.find(params[:id])
+    @customer = current_customer
   end
 
   def update
